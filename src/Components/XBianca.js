e5b671bdb8daad544c119fc077519a5dc4f288e1
Jeng <jason.eng222@gmail.com>
2023-02-03 23:50:56 -0500

    Working on XB and course on the side

--- src/Components/XBianca.js
+++ src/Components/XBianca.js
@@ -1,7 +1,8 @@
-
+import "../App.css"
 const XBianca = (
 <div>
-    <img src="https://media.discordapp.net/attachments/650900745024962581/1009013546157355058/cbi1.png" />
-</div>
+    <div>XBianca Guide Courtesy of Patch</div>
+    <img className="center-Image" alt="XBianca 1" src="https://media.discordapp.net/attachments/650900745024962581/1009013546157355058/cbi1.png" />
+</div>  
 )
 export default XBianca 
--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
 import todoReducer from './Stores/todosSlice'
 
-
-
 export const store = configureStore({
     reducer: {
         todos: todoReducer
