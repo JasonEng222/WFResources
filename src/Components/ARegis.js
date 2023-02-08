87df16fd888afd5904cd4ff94d4be7d70f967d38
Jeng <jason.eng222@gmail.com>
2023-02-07 22:20:28 -0500

    Worked on ARegis Page

---
+++ src/Components/ARegis.js
@@ -0,0 +1,11 @@
+import "../App.css"
+const ARegis = (
+<div>
+    <div>ARegis Guide Courtesy of Patch</div>
+    <img className="center-Image" alt="ARegis 1" src="https://media.discordapp.net/attachments/650900769708441611/1002473923084034068/regis1.png" />
+    <img className="center-Image" alt="ARegis 2" src="https://media.discordapp.net/attachments/650900769708441611/1002473923461517392/regis2.png" />
+    <img className="center-Image" alt="ARegis 3" src="https://cdn.discordapp.com/attachments/650900769708441611/1002473923847405588/regis3.png" />
+    <img className="center-Image" alt="ARegis 4" src="https://cdn.discordapp.com/attachments/650900769708441611/1002473924229083228/regis4.png" />
+</div>  
+)
+export default ARegis 
--- src/Components/XBianca.js
+++ src/Components/XBianca.js
@@ -3,9 +3,9 @@ const XBianca = (
 <div>
     <div>XBianca Guide Courtesy of Patch</div>
     <img className="center-Image" alt="XBianca 1" src="https://media.discordapp.net/attachments/650900745024962581/1009013546157355058/cbi1.png" />
-    <img className="center-Image" alt="XBianca 1" src="https://media.discordapp.net/attachments/650900745024962581/1009013546627104809/cbi2.png" />
-    <img className="center-Image" alt="XBianca 1" src="https://media.discordapp.net/attachments/650900745024962581/1009013547029762098/cbi3.png" />
-    <img className="center-Image" alt="XBianca 1" src="https://media.discordapp.net/attachments/650900745024962581/1009013548065759312/cbi4.png" />
+    <img className="center-Image" alt="XBianca 2" src="https://media.discordapp.net/attachments/650900745024962581/1009013546627104809/cbi2.png" />
+    <img className="center-Image" alt="XBianca 3" src="https://media.discordapp.net/attachments/650900745024962581/1009013547029762098/cbi3.png" />
+    <img className="center-Image" alt="XBianca 4" src="https://media.discordapp.net/attachments/650900745024962581/1009013548065759312/cbi4.png" />
 </div>  
 )
 export default XBianca 
